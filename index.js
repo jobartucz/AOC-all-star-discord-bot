4e705d10ccd4ffb357b112c100b56c8ce3258486
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2021-12-10 18:35:29 -0600

    Auto add/remove people from All stars channel
    (may not work, it's not fully tested)

--- index.js
+++ index.js
@@ -6,63 +6,95 @@ var channels = require("./channels.js")
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS"] });
 Object.filter = (obj, predicate) =>
   Object.fromEntries(Object.entries(obj).filter(predicate));
+
+function getCompletedStars(member) {
+  return Object.keys(Object.filter(member.completion_day_level, ([key, value]) => value.hasOwnProperty("1") && value.hasOwnProperty("2")))
+}
+function convertTZ(date, tzString) {
+  return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", { timeZone: tzString }));
+}
+
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
   console.log('Ready!');
+
   client.user.setActivity(`you solve aoc`, { type: 'WATCHING' });
 
-  function getCompletedStars(member) {
-    return Object.keys(Object.filter(member.completion_day_level, ([key, value]) => value.hasOwnProperty("1") && value.hasOwnProperty("2")))
-  }
+
   setInterval(() => {
-  get("https://saturn.rochesterschools.org/python/AOCbot/data_file.json").then((resp) => {
-    get("https://saturn.rochesterschools.org/python/AOCbot/users.json").then((resp2) => {
-      var members = Object.values(resp.data.members)
-      var discords = {};
-      for (var name in resp2.data) {
-        var discord = resp2.data[name]["If you are participating in the RCC Discord server, you will be automatically added to specific channels when you complete stars. You can join here: https://discord.gg/hsN92V4  - Please enter your Discord username so we can verify you."]
-        if (discord) discords[name] = discord
-      }
-      let list = client.users.cache
-      //  let guild = client.guilds.cache.get("918880187318403073");
+    var d = new Date();
+    get("https://saturn.rochesterschools.org/python/AOCbot/data_file.json").then((resp) => {
+      get("https://saturn.rochesterschools.org/python/AOCbot/users.json").then((resp2) => {
+        var members = Object.values(resp.data.members)
+        var discords = {};
+        for (var name in resp2.data) {
+          var discord = resp2.data[name]["If you are participating in the RCC Discord server, you will be automatically added to specific channels when you complete stars. You can join here: https://discord.gg/hsN92V4  - Please enter your Discord username so we can verify you."]
+          if (discord) discords[name] = discord
+        }
+        let list = client.users.cache
+
+        members.forEach((member) => {
 
 
-      members.forEach((member) => {
+          var duser = list.find(u => discords[member.name] == u.tag)
+          if (duser) {
 
-        var duser = list.find(u => discords[member.name] == u.tag)
-        if (duser) {
-          var user = { discord: { id: duser.id, tag: discords[member.name] }, completed: getCompletedStars(member), name: member.name, id: member.id }
+            var user = { discord: { id: duser.id, tag: discords[member.name] }, completed: getCompletedStars(member), name: member.name, id: member.id }
 
-          user.completed.forEach((completed) => {
-            var channel = client.channels.cache.find(x => x.id.toString() == channels[completed])
-            if (channel) {
-              if(!channel.permissionOverwrites.cache.find(x=>x.id == user.discord.id)) {
-              channel.permissionOverwrites.create(duser, {
-                SEND_MESSAGES: true,
-                VIEW_CHANNEL: true
-              });
-              channel.send(user.name+" has completed Day "+completed+"!")
-              } else {
-                //already is in channel
+            var now = convertTZ(new Date(), "America/Chicago")
+            var aocStart = convertTZ(new Date("2021/12/01 11:0:0 +0000"), "America/Chicago")
+            var puzzlesNeeded = Math.floor((now.getTime() - aocStart.getTime()) / (1000 * 3600 * 24)) + 1;
+            var allChannel = client.channels.cache.find(x => x.id.toString() == channels.all)
+            if (user.completed.length == puzzlesNeeded) {
+              if (!allChannel.permissionOverwrites.cache.find(x => x.id == user.discord.id)) {
+                allChannel.permissionOverwrites.create(duser, {
+                  SEND_MESSAGES: true,
+                  VIEW_CHANNEL: true
+                });
+                allChannel.send(user.name + " has completed all the problems")
               }
-              
+            } else if (allChannel.permissionOverwrites.cache.find(x => x.id == user.discord.id)) {
+              allChannel.permissionOverwrites.delete(user.discord.id)
             }
 
-          })
-          
 
 
+
+            user.completed.forEach((completed) => {
+              var channel = client.channels.cache.find(x => x.id.toString() == channels[completed])
+              if (channel) {
+                if (!channel.permissionOverwrites.cache.find(x => x.id == user.discord.id)) {
+                  channel.permissionOverwrites.create(duser, {
+                    SEND_MESSAGES: true,
+                    VIEW_CHANNEL: true
+                  });
+                  channel.send(user.name + " has completed Day " + completed + "!")
+                } else {
+                  //already is in channel
+                }
+
+              }
+
+            })
+
+
+
+          }
+
+        })
+
+        if (d.getHours() - 6 == 11 && d.getMinutes == 0) {
+          var channel = client.channels.cache.find(x => x.id.toString() == channels.all)
+          channel.send("A new puzzle has come out! Everyone has been removed from this channel.")
         }
 
+
+
+
       })
-
-
-
-
     })
-  })
 
-  },60000)
+  }, 30000)
 
 
 });
